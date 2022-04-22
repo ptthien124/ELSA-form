41ccbf8b68f02fad057cdcdd0ed2b07fe5d33223
ptthien124 <ptthien124@gmail.com>
2022-04-22 13:32:18 +0700

    updates

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -9,13 +9,15 @@ import "../styles/css/logo.css";
 function Logo() {
   return (
     <div className="logo">
-      <img className="logo__img" src={logoImage} alt="" />
-      <div className="logo__container">
-        <img src={E} alt="" />
-        <img src={L} alt="" />
-        <img src={S} alt="" />
-        <img src={A} alt="" />
-      </div>
+      <a href="/">
+        <img className="logo__img" src={logoImage} alt="" />
+        <div className="logo__container">
+          <img src={E} alt="" />
+          <img src={L} alt="" />
+          <img src={S} alt="" />
+          <img src={A} alt="" />
+        </div>
+      </a>
     </div>
   );
 }
