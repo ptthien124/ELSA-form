681992008fdea11e9b916c183f49b42964a2e3cd
ptthien124 <ptthien124@gmail.com>
2022-04-22 14:24:46 +0700

    updates bottom

--- src/components/Bottom.js
+++ src/components/Bottom.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Phones from "../public/images/bottom/phones.png";
 import "../styles/css/bottom.css";
 
 function Bottom() {
@@ -10,10 +9,62 @@ function Bottom() {
   };
   return (
     <div className="bottom">
-      <img src={Phones} alt="" />
-      <div className="bottom__container">
-        <button onClick={handleBottomBtnClick}>Đăng ký ngay</button>
+      <div className="heading">
+        <h2>CÁ NHÂN HOÁ LỘ TRÌNH HỌC</h2>
+        <p>Làm bài kiểm tra đầu vào với ELSA trong 5 phút và bạn sẽ có:</p>
       </div>
+      <div className="body">
+        <div className="item">
+          <div className="img">
+            <img
+              src="https://vn.elsaspeak.com/wp-content/webp-express/webp-images/doc-root/wp-content/themes/theme_two_years/images/section-02-1.png.webp"
+              alt=""
+            />
+          </div>
+          <div className="content">
+            Trí tuệ nhân tạo trả về bạn số điểm <b>“CHUẨN BẢN XỨ"</b>
+          </div>
+        </div>
+        <div className="item">
+          <div className="img">
+            <img
+              src="https://vn.elsaspeak.com/wp-content/webp-express/webp-images/doc-root/wp-content/themes/theme_two_years/images/section-02-2.png.webp"
+              alt=""
+            />
+          </div>
+          <div className="content">
+            <b>NGAY LẬP TỨC</b>, bạn biết mình phát âm sai ở đâu qua những gam
+            màu trực quan sinh động.
+          </div>
+        </div>
+        <div className="item">
+          <div className="img">
+            <img
+              src="https://vn.elsaspeak.com/wp-content/webp-express/webp-images/doc-root/wp-content/themes/theme_two_years/images/section-02-3.png.webp"
+              alt=""
+            />
+          </div>
+          <div className="content">
+            <b>CÔNG NGHỆ NHẬN DIỆN</b> giọng nói chuẩn đến từng âm tiết.
+          </div>
+        </div>
+        <div className="item">
+          <div className="img">
+            <img
+              src="https://vn.elsaspeak.com/wp-content/webp-express/webp-images/doc-root/wp-content/themes/theme_two_years/images/section-02-4.png.webp"
+              alt=""
+            />
+          </div>
+          <div className="content">
+            ELSA sẽ <b>LẬP LỘ TRÌNH HỌC CÁ NHÂN HOÁ</b> dựa trên các chỉ số bài
+            kiểm tra đầu vào của bạn.
+          </div>
+        </div>
+      </div>
+
+      <button className="bottom__btn" onClick={handleBottomBtnClick}>
+        Đăng ký ngay
+      </button>
     </div>
   );
 }
